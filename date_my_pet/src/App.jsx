a3afa1ad766d2956281913dd4602a5d55807b303
laura baratier <laura.baratierpro@gmail.com>
2024-04-03 09:43:26 +0200

    Css page

--- date_my_pet/src/App.jsx
+++ date_my_pet/src/App.jsx
@@ -1,34 +1,65 @@
 import { useState } from "react";
+import React from "react";
+//link STYLESHEET//
 import "./App.css";
-import Footer from "./components/Footer";
+//import components//
 import NavBar from "./components/NavBar";
 import MyProfil from "./components/MyProfil";
-import Jazzy from "./assets/Jazzy.jpg";
-import Likes from "./components/Likes";
-
 import Matching from "./components/Matching";
-import React from "react";
-//pet images
-import Jazzy from "./assets/Jazzy.png";
+import Likes from "./components/Likes";
+import Footer from "./components/Footer";
 
+//pet img
+import Jazzy from "./assets/Jazzy.jpg";
+// import Tessla from "./assets/tessla.jpg";
+
+const petList = [
+  {
+    petImage: Jazzy,
+
+    petName: "Jazzy",
+    petRace: "Sacré de Birmanie",
+    petAge: "2 ans",
+    petLoof: "no",
+
+    petDescription:
+      "Je suis une belle femelle, qui cherche un chat affectueux pour co-créer mes héritiers, je suis joueuse et sociable, je recherche un mâle pour faire des battards trop mimis, de préférence un roux.bladkzfgevceevf",
+  },
+
+  {
+    petImage: Jazzy,
+
+    petName: "Tessla",
+    petRace: "Sacré de Birmanie",
+    petAge: "2 ans",
+    petLoof: "yes",
+
+    petDescription:
+      "Belle et charmante. Elle est dsiponible toutes les 3 semaines,Belle et charmante. Elle est dsiponible toutes les 3 semaines",
+  },
+
+  {
+    petImage: Jazzy,
+
+    petName: "Urssaf",
+    petRace: "Sphynx",
+    petAge: "2 ans",
+    petLoof: "no",
+
+    petDescription:
+      "Je suis sans poil mais vous serez au poil avec moi,Je suis sans poil mais vous serez au poil avec moi,Je suis sans poil mais vous serez au poil avec moi",
+  },
+];
+
+const Profile = {
+  myImg: Jazzy,
+  myPet: "Jazzy",
+};
 function App() {
   const [petIndex, setPetIndex] = useState(0);
 
-  const petList = [
-    {
-      petImage: Jazzy,
-      petName: "Jazzy",
-      petDescription: "La plus belle au monde!",
-    },
-    {
-      petImage: Jazzy,
-      petName: "Flash",
-      petDescription: "beau gosse",
-    },
-  ];
-
   const handleClickButton = () => {
-    if (petIndex < petList.lenght - 1) {
+    if (petIndex < petList.length - 1) {
       setPetIndex(petIndex + 1);
     }
   };
@@ -38,12 +69,10 @@ function App() {
   return (
     <>
       <NavBar></NavBar>
-      <MyProfil img={Profile.myImg} petname={Profile.myPet}></MyProfil>
-      <div class="Matching">
-        <Matching pet={petList[petIndex]} clickButton={handleClickButton} />
-        <Likes matchcount={matchCount}></Likes>
-        <Footer></Footer>
-      </div>
+      <MyProfil img={Profile.myImg} petname={Profile.myPet} />
+      <Matching pet={petList[petIndex]} clickButton={handleClickButton} />
+      <Likes matchcount={matchCount}></Likes>
+      <Footer></Footer>
     </>
   );
 }
